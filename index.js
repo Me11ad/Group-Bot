446238e6425d63eb185386872273c451512a11e5
Me11ad <vlad505200483@gmail.com>
2023-06-16 14:15:57 +0500

    Добавка

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const {Telegraf, Markup} = require('telegraf');
 
-const bot = new Telegraf('BOT_TOKEN');
+const bot = new Telegraf('6084139426:AAFPjW6DxwF6HGFZuIqVu0ZeRO-mHCCvEBo');
 
 bot.use(async (ctx) => {
     await ctx.reply(JSON.stringify(ctx.update, null, 2));
 });
 
 bot.on('text', (ctx) => ctx.reply(`Текст "${ctx.update.message.text}" не поддерживается.`))
+
+bot.use(async (ctx) => {
+    await ctx.reply('Что нужно сделать?', Markup
+        .keyboard([
+            ['Добавить студента', 'Показать список студентов', 'Удалить студента'],
+        ]).resize()
+    )
+});
 
 
 bot.launch().then(() => console.log('Started'));
