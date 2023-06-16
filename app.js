5ecd1334b0ac91fa6f09c915a16887810805a0e1
Mellad <vlad505200483@gmail.com>
2023-06-16 22:49:12 +0300

    Почти доделан

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import { PORT, TOKEN } from './config.js'
 import Telegraf from 'telegraf'
-//import { getMainMenu } from './keyboard.js'
+import session from 'telegraf/session.js'
+import { getMainMenu, yesNoKeyboard} from './keyboard.js'
+import { getStudents, addStudent, deleteStudent } from './db.js'
 
 const app = express()
 const bot = new Telegraf(TOKEN)
+
+bot.use(session())
 
 bot.start(ctx => {
     ctx.replyWithHTML('Приветствую в <b>GroupBot</b>\n\n'+
@@ -16,15 +20,50 @@ bot.start(ctx => {
     //ctx.reply('Тут вы сможете добавить студента')
 //})
 
-//bot.hears('Список студентов', ctx => {
-    //ctx.reply('Тут будет список студентов')
-//})
+bot.hears('Список студентов', async ctx => {
+    const students = await getStudents()
+    let result = ''
+
+    for (let i = 0; i < students.length; i++) {
+        result = result + `[${i+1}] ${tasks[i]}\n`
+    }
+
+    ctx.replyWithHTML{
+        '<b>Список студентов:</b>\n\n'+
+        `${result}`
+    }
+})
+
+bot.hears('Удалить студента', ctx => {
+    ctx.replyWithHTML(
+        'Введите фразу <i>"удалить `порядковый номер студента`"</i>, чтобы удалить сообщение,'+
+        'например, <b>"удалить 3"</b>:'
+    )
+})
+
+bot.hears(/^удалить\s(\d+)$/, ctx => {
+    const id = Number(+/\d+/.exec(ctx.message.text)) - 1
+    deleteStudent(id)
+    ctx.reply('Студент успешно удалён')
+})
+
 bot.on('text', ctx => {
+    ctx.session.studentText = ctx.message.text
+
     ctx.replyWithHTML(
         `Вы действительно хотите добавить студента:\n\n`+
         `<i>${ctx.message.text}</i>`,
          yesNoKeyboard()
     )
+})
+
+bot.action(['yes', 'no'], ctx => {
+    if (ctx.callbackQuery.data === 'yes') {
+        addTask('сюда будем передавать имя студента')
+        ctx.editMessageText('Студент успешно добавлен')
+    } else {
+        ctx.deleteMessage()
+    }
 })
 
 bot.launch()
--- db.js
+++ db.js
@@ -1,1 +1,16 @@
-export let taskList = []
+export let StudentList = []
+
+export function getStudents() {
+    return new Promisse((resolve) => {
+        setTimeout(() => {
+            resolve(StudentList)
+        }, 500)
+    })
+}
+export function addStudent(text) {
+    studentList.push(text)
+}
+
+export function deleteStudent(id) {
+    studentList.splice(id, 1)
+}
